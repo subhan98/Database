6ac9d8cd0e640d2ce14bce6dbd207a011a75e73a
Muhammad Subhan <subhanansari586@gmail.com>
2019-11-26 06:27:39 +0000

    Bookmarks manager 2nd challange completed.

---
+++ app.rb
@@ -0,0 +1,8 @@
+require 'sinatra/base'
+
+class BookmarkManager < Sinatra::Base
+  get '/' do
+  'Hello world.'
+end
+#run! if app_file == $0
+end
---
+++ spec/features/features_test_spec.rb
@@ -0,0 +1,7 @@
+require 'capybara/rspec'
+  feature 'Viewing bookmarks' do
+  scenario 'visiting the index page' do
+    visit('/')
+    expect(page).to have_content 'Bookmark Manager'
+ end
+end
